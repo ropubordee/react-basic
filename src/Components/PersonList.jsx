689aaddf372c276d8853aa837bc7680360825fb1
devpubordee <boszza1357009@gmail.com>
2025-10-20 03:16:31 +0700

    lnline & Dynamic Style

--- src/Components/PersonList.jsx
+++ src/Components/PersonList.jsx
@@ -9,17 +9,23 @@ const PersonList = () => {
     { id: 3, name: "โจ้", gender: "ชาย" },
     { id: 4, name: "พลอย", gender: "หญิง" },
   ]);
+
+  const myStyle = {
+    color: "red",
+    fontSize: "30px",
+  };
+
   const [show, setShow] = useState(true);
   return (
     <div className="container">
       <div className="header">
-        <h2>จำนวนประชากร {data.length}</h2>
+        <h2 style={myStyle}>จำนวนประชากร {data.length}</h2>
         <button onClick={() => setShow(!show)}>{show ? "ซ่อน" : "แสดง"}</button>
       </div>
       <ul>
         {show &&
           data.map((item, index) => (
-            <li key={index}>
+            <li key={index} style={{borderStyle :'solid',borderColor : item.gender =='ชาย' ? 'green' : 'pink'}}>
               <img
                 src={item.gender == "ชาย" ? boy : girl}
                 alt="imageicon"
