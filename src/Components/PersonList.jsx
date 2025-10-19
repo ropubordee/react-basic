8f471e8cc1f661b76b6ef5cbfa9e6460d076af8c
devpubordee <boszza1357009@gmail.com>
2025-10-19 14:28:57 +0700

    Image

--- src/Components/PersonList.jsx
+++ src/Components/PersonList.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-
+import boy from "../assets/boy.svg";
+import girl from '../assets/girl.svg'
 const PersonList = () => {
   const [data, setData] = useState([
     { id: 1, name: "pubordee", gender: "ชาย" },
     { id: 2, name: "น้ำ", gender: "หญิง" },
     { id: 3, name: "โจ้", gender: "ชาย" },
+    { id: 4, name: "พลอย", gender: "หญิง" },
   ]);
   const [show, setShow] = useState(true);
   return (
@@ -15,6 +17,8 @@ const PersonList = () => {
         {show &&
           data.map((item, index) => (
             <li key={index}>
+              <img src={item.gender == 'ชาย' ? boy : girl} alt="imageicon" width={50} height={50} />
+
               <h3>
                 {item.id}| {item.name} | {item.gender}
               </h3>
